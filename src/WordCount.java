14c04efbd75d309a13cc366c748302410041ee43
NZJ-Jimmy <JimmyLeung@m.scnu.edu.cn>
2024-11-10 15:44:38 +0800

    Decoupling WordCount

--- src/WordCount.java
+++ src/WordCount.java
@@ -1,18 +1,35 @@
-import java.io.IOException;  
+import java.io.IOException;
 import java.util.StringTokenizer;
-import org.apache.hadoop.conf.Configuration;  
-import org.apache.hadoop.fs.Path;  
-import org.apache.hadoop.io.IntWritable;  
-import org.apache.hadoop.io.Text;  
-import org.apache.hadoop.mapreduce.Job;  
-import org.apache.hadoop.mapreduce.Mapper;  
-import org.apache.hadoop.mapreduce.Reducer;  
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;  
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;  
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
-
 public class WordCount {
+
+    /**
+     * MyMapper is a Mapper class that extends the Hadoop Mapper class.
+     * It processes input key-value pairs to generate a set of intermediate key-value pairs.
+     * 
+     * The map method takes an input key-value pair and tokenizes the value (which is a line of text).
+     * For each token (word) in the line, it writes the word and a count of one to the context.
+     * 
+     * Key: Object (input key, not used in this implementation)
+     * Value: Text (a line of text from the input)
+     * 
+     * Output Key: Text (a word from the input line)
+     * Output Value: IntWritable (the count of the word, which is always 1 in this case)
+     * 
+     * @param key the input key (not used in this implementation)
+     * @param value the input value (a line of text)
+     * @param context the context to write the output key-value pairs
+     */
     public static class MyMapper extends Mapper<Object, Text, Text, IntWritable> {
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
@@ -26,6 +43,15 @@ public class WordCount {
         }
     }
 
+    /**
+     * A Reducer class that extends the Hadoop Reducer class.
+     * 
+     * The reduce method sums up all the IntWritable values associated with a given key and writes the key and the sum to the context.
+     * 
+     * @param key The input key.
+     * @param values An iterable list of IntWritable values associated with the key.
+     * @param context The context to write the output key-value pair.
+     */
     public static class MyReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
         private IntWritable result = new IntWritable();
 
@@ -40,6 +66,37 @@ public class WordCount {
         }
     }
 
+    /**
+     * A combiner class that extends the {@link MyReducer} class.
+     * 
+     * It is an alias of {@link MyReducer}. This can help in reducing the amount of
+     * data transferred across the network. Accelarates the process of reducing the
+     * data.
+     */
+    public static class MyCombiner extends MyReducer {
+    }
+
+    /**
+     * Configures and returns a new Hadoop Job for counting words.
+     *
+     * @param conf   the Hadoop configuration to use for the job
+     * @param input  the input path for the job
+     * @param output the output path for the job
+     * @return a configured Job instance for word counting
+     */
+    public static Job getJob(Configuration conf, Path input, Path output) throws IOException {
+        Job job = new Job(conf, "word count");
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(MyMapper.class);
+        job.setCombinerClass(MyCombiner.class);
+        job.setReducerClass(MyReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, input);
+        FileOutputFormat.setOutputPath(job, output);
+        return job;
+    }
+
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
@@ -47,14 +104,7 @@ public class WordCount {
             System.err.println("Usage: wordcount <in> <out>");
             System.exit(2);
         }
-        Job job = new Job(conf, "word count");
-        job.setJarByClass(WordCount.class);
-        job.setMapperClass(MyMapper.class);
-        job.setReducerClass(MyReducer.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
-        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+        Job job = getJob(conf, new Path(otherArgs[0]), new Path(otherArgs[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
